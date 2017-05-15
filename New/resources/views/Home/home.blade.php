4180718cdb16a19325ea53ab2d2210a1228399b1
Gabriel Silva Tosetti <gabriel.tosetti@hotmail.com>
2017-05-15 17:48:35 -0300

    Commit final para a apresentação

--- New/resources/views/Home/home.blade.php
+++ New/resources/views/Home/home.blade.php
@@ -9,10 +9,9 @@
   </div>
   <div class="panel panel-default">
     <div class="panel-heading">
-      Home maneira
+      Home
     </div>
     <div class="panel-body">
-      <h1> ~~coisas maneiras~~ </h1>
 
     </div>
 
